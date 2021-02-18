eb0482ea2a904a3e2d4ca586598f9de105eccf1a
Ivan Pepelko <ivan.pepelko@gmail.com>
2021-02-18 12:06:28 +0100

    Initialize datatables on tables

--- assets/app.js
+++ assets/app.js
@@ -8,8 +8,15 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.css';
-
-// start the Stimulus application
+import 'datatables.net-bs4/css/dataTables.bootstrap4.css';
 import './bootstrap';
 
+// start the Stimulus application
 import 'bootstrap/dist/js/bootstrap';
+import 'datatables.net-bs4/js/dataTables.bootstrap4';
+
+const $ = require('jquery');
+
+$(function () {
+    $('table.datatable').dataTable();
+});
